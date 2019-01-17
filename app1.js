5d10f64d8c37985f206c603a1f50711b255b31e4
ThushaSiva <32883239+ThushaSiva@users.noreply.github.com>
2019-01-17 15:21:22 -0500

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,4 +1,3 @@
-
 //this is the 1st way.
 $(document).ready(function(){
     $('#upload').click(function(){
@@ -21,23 +20,4 @@ $(document).ready(function(){
             reader.readAsText(csvFile);
         }
     });
-});
-
-
-/* $("#filename").change(function(e){
-	var ext = $("#filename").val().split(".").pop().toLowerCase();
-
-	if($.inArray(ext, ["csv"]) == -1) {
-		alert('Upload CSV');
-		return false;
-	}
-
-	if(e.target.files != undefined){
-		var reader = new FileReader();
-		reader.onload = function(e){
-			csvResult = e.target.result.split(/\r|\n|\r\n/);
-			$('.csv').append(csvResult);
-		}
-		reader.readAsText(e.target.files.item(0));
-	}
 });
