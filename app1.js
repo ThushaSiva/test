8749063d8167dfdfe441f7cda6404146a0d0831e
ThushaSiva <32883239+ThushaSiva@users.noreply.github.com>
2019-01-17 15:24:17 -0500

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,23 +1,17 @@
-//this is the 1st way.
-$(document).ready(function(){
-    $('#upload').click(function(){
+$("#filename").change(function(e){
+	var ext = $("#filename").val().split(".").pop().toLowerCase();
 
-        var csv = $('#filename');
-        var csvFile = csv[0].files[0];
-        var ext = csv.val().split(".").pop().toLowerCase();
+	if($.inArray(ext, ["csv"]) == -1) {
+		alert('Upload CSV');
+		return false;
+	}
 
-        if($.inArray(ext, ["csv"]) === -1){
-            alert('upload csv');
-            return false;
-        }
-        if(csvFile != undefined){
-            reader = new FileReader();
-            reader.onload = function(e){
-
-                csvResult = e.target.result.split(/\r|\n|\r\n/);
-                $('.csv').append(csvResult);
-            }
-            reader.readAsText(csvFile);
-        }
-    });
+	if(e.target.files != undefined){
+		var reader = new FileReader();
+		reader.onload = function(e){
+			csvResult = e.target.result.split(/\r|\n|\r\n/);
+			$('.csv').append(csvResult);
+		}
+		reader.readAsText(e.target.files.item(0));
+	}
 });
