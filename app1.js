e40b388962c1aeb1e521c36cfdba8ba0a28bbe7e
ThushaSiva <32883239+ThushaSiva@users.noreply.github.com>
2019-01-17 15:30:34 -0500

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,17 +1,23 @@
-$("#filename").change(function(e){
-	var ext = $("#filename").val().split(".").pop().toLowerCase();
+//this is the 1st way.
+$(document).ready(function(){
+    $('#upload').click(function(){
 
-	if($.inArray(ext, ["csv"]) == -1) {
-		alert('Upload CSV');
-		return false;
-	}
+        var csv = $('#filename');
+        var csvFile = csv[0].files[0];
+        var ext = csv.val().split(".").pop().toLowerCase();
 
-	if(e.target.files != undefined){
-		var reader = new FileReader();
-		reader.onload = function(e){
-			csvResult = e.target.result.split(/\r|\n|\r\n/);
-			$('.csv').append(csvResult);
-		}
-		reader.readAsText(e.target.files.item(0));
-	}
+        if($.inArray(ext, ["csv"]) === -1){
+            alert('upload csv');
+            return false;
+        }
+        if(csvFile != undefined){
+            reader = new FileReader();
+            reader.onload = function(e){
+
+                csvResult = e.target.result.split(/\r|\n|\r\n/);
+                $('.csv').append(csvResult);
+            }
+            reader.readAsText(csvFile);
+        }
+    });
 });
