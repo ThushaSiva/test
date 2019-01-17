d73d9e1cba615ba8ab5c37c4550c7106a878e9df
ThushaSiva <32883239+ThushaSiva@users.noreply.github.com>
2019-01-17 15:10:44 -0500

    Update app1.js

--- app1.js
+++ app1.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 });
 */
 
-/* $("#filename").change(function(e){
+ $("#filename").change(function(e){
 	var ext = $("#filename").val().split(".").pop().toLowerCase();
 
 	if($.inArray(ext, ["csv"]) == -1) {
@@ -41,39 +41,3 @@ $(document).ready(function(){
 		reader.readAsText(e.target.files.item(0));
 	}
 });
-*/
-
-var tabulate = function (data,columns) {
-  var table = d3.select('body').append('table')
-	var thead = table.append('thead')
-	var tbody = table.append('tbody')
-
-	thead.append('tr')
-	  .selectAll('th')
-	    .data(columns)
-	    .enter()
-	  .append('th')
-	    .text(function (d) { return d })
-
-	var rows = tbody.selectAll('tr')
-	    .data(data)
-	    .enter()
-	  .append('tr')
-
-	var cells = rows.selectAll('td')
-	    .data(function(row) {
-	    	return columns.map(function (column) {
-	    		return { column: column, value: row[column] }
-	      })
-      })
-      .enter()
-    .append('td')
-      .text(function (d) { return d.value })
-
-  return table;
-}
-
-d3.csv('data.csv',function (data) {
-	var columns = ['variable','aror','asd','maxdd']
-  tabulate(data,columns)
-})
